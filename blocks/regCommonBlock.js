da451baf8c5cc8ff710f5126ef937146477b4ec9
Pantelis <pant.kakavas@gmail.com>
2024-01-18 19:20:30 +0200

    Addition of lookaheads and some predefined common regex searches

---
+++ blocks/regCommonBlock.js
@@ -0,0 +1,37 @@
+var regCommonBlock = {
+  type: "regCommon",
+  category: "Regular Expressions",
+  unix_description: [
+    {
+      email : "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}",
+      URL : "(?:https?:\/\/|www\.)[^\s]+",
+      IP : "^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$",
+      MMDDYYYY : "(0[1-9]|1[0-2])[\/-](0[1-9]|[12][0-9]|3[01])[\/-](19|20)\d{2}",
+      DDMMYYYY : "(0[1-9]|[12][0-9]|3[01])[\/-](0[1-9]|1[0-2])[\/-](19|20)\d{2}",
+      YYYYMMDD : "(19|20)\d{2}[\/-](0[1-9]|1[0-2])[\/-](0[1-9]|[12][0-9]|3[01])"
+    }
+  ],
+  message0: "Common searches",
+  message1: "Match %1",
+  args1: [
+    {
+      type: "field_dropdown",
+      name: "reg_common",
+      options: [
+        [ "email address", "email" ],
+        [ "URL", "URL" ],
+        [ "IP address", "IP" ],
+        [ "date in MM/DD/YYYY Format", "MMDDYYYY" ],
+        [ "date in DD/MM/YYYY Format", "DDMMYYYY"],
+        [ "date in YYYY/MM/DD Format", "YYYYMMDD"]
+      ]
+    }
+  ],
+  colour: 630,
+  previousStatement: "Action",
+  nextStatement: "Action",
+  tooltip: "Common regular expression searches",
+  helpUrl: "" // URL to further information or documentation.
+};
+
+Blockly.defineBlocksWithJsonArray([regCommonBlock]);
---
+++ blocks/regLookaheadBlock.js
@@ -0,0 +1,35 @@
+var regLookaheadBlock = {
+  type: "regLookahead",
+  category: "Regular Expressions",
+  unix_description: [
+    {
+      positive : "(?= patt)",
+      negative : "(?! patt)"
+    }
+  ],
+  message0: "pattern %1 followed by %2",
+  args0: [
+    {
+      type: "field_dropdown",
+      name: "reg_lookahead",
+      options: [
+        [ "is", "positive" ],
+        [ "is not", "negative" ]
+      ]
+    },
+    {
+      type: "input_value",
+      name: "regPattern",
+      check: "String" 
+    }
+
+
+  ],
+  colour: 510,
+  previousStatement: "Action",
+  nextStatement: "Action",
+  tooltip: "Match a pattern that follows or doesn't follow another pattern",
+  helpUrl: "" // URL to further information or documentation.
+};
+
+Blockly.defineBlocksWithJsonArray([regLookaheadBlock]);
